c93a950c9bf494d8c181534b6308d873575f5094
notnonameexe <56001156+notnonameexe@users.noreply.github.com>
2023-06-18 01:59:55 +0300

    First

---
+++ Assets/Скрипты/Half.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Half : MonoBehaviour
+{
+
+	public GameObject LapCompleteTrig;
+	public GameObject HalfLapTrig;
+
+	void OnTriggerEnter()
+	{
+		LapCompleteTrig.SetActive(true);
+		HalfLapTrig.SetActive(false);
+	}
+}
